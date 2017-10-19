28a4c926d50b536ebd313360ff3169f43807db08
Jeroen Mies <jeroen.mies@gmail.com>
2017-10-19 14:10:19 +0200

    Setup app components

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class PostsShow extends Component {
+    render() {
+        return (
+            <div>
+                Show Post
+            </div>
+        );
+    }
+}
+
+export default PostsShow;
--- src/index.js
+++ src/index.js
@@ -2,31 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from "./components/posts_index";
+import PostsShow from "./components/posts_show";
+import PostsNew from "./components/posts_new";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-    render() {
-        return <div>Hello!</div>
-    }
-}
-
-class Goodbye extends React.Component {
-    render() {
-        return <div>Goodbye!</div>
-    }
-}
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <div>
-            <Route path="/hello" component={Hello} />
-            <Route path="/goodbye" component={Goodbye} />
+                <Route path="/" component={PostsIndex}/>
+                <Route path="/posts/:id" component={PostsShow}/>
+                <Route path="/posts/new" component={PostsNew}/>
             </div>
         </BrowserRouter>
     </Provider>
